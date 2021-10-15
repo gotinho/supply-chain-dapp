52a2055067ac84b247f276c2b8564c341bdb5704
Igor Alexandre Goto <gotinho@users.noreply.github.com>
2021-10-14 22:33:32 -0300

    implemented 2nd Test

--- test/TestSupplychain.js
+++ test/TestSupplychain.js
@@ -57,6 +57,8 @@ contract('SupplyChain', function(accounts) {
             eventEmitted = true
         })
 
+        await supplyChain.addFarmer(originFarmerID);
+
         // Mark an item as Harvested by calling function harvestItem()
         await supplyChain.harvestItem(upc, originFarmerID, originFarmName, originFarmInformation, originFarmLatitude, originFarmLongitude, productNotes)
 
@@ -82,19 +84,24 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
         
         // Declare and Initialize a variable for event
-        
+        let eventEmitted = false;
         
         // Watch the emitted event Processed()
+        supplyChain.Processed().on('data', event => {
+            eventEmitted = true;
+        });
         
 
         // Mark an item as Processed by calling function processtItem()
+        await supplyChain.processItem(upc, {from: originFarmerID});
         
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
-        
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc)
 
         // Verify the result set
-        
+        assert.equal(resultBufferTwo[5], 1, 'Error: Invalid item State')
+        assert.equal(eventEmitted, true, 'Invalid event emitted')
     })    
 
     // 3rd Test
